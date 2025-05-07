a0cec98561bc3f6d084932be41e577bbea8bb8b9
Dipti Kumari <48diptikumari2500@gmail.com>
2025-05-08 03:11:41 +0530

    Updated the content of the JSON file used for the /api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import pymongo
 import os
 from dotenv import load_dotenv
@@ -31,6 +31,18 @@ def index():
     # Fetch all notes from MongoDB
     notes = collection.find()
     return render_template('index.html', notes=notes)
+
+@app.route('/api', methods=['GET'])
+def api():
+    notes = collection.find()
+    notes_list = []
+    for note in notes:
+        notes_list.append({
+            "id": str(note.get('_id')),
+            "title": note.get('title'),
+            "content": note.get('content')
+        })
+    return jsonify(notes_list)
 
 # Run the app
 if __name__ == '__main__':
